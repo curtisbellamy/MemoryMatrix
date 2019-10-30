be90b4fa694f0238f8660865c579970a6287012f
curtisbellamy <35937828+curtisbellamy@users.noreply.github.com>
2019-10-30 13:29:23 -0700

    migrated to mongodb

--- app.js
+++ app.js
@@ -4,26 +4,10 @@ const path = require('path');
 const router = express.Router();
 const bodyParser = require('body-parser')
 
-var firebase = require("firebase/app");
+const MongoClient = require('mongodb').MongoClient;
 
-require("firebase/auth");
-require("firebase/database");
-const firebaseConfig = {
-    apiKey: "AIzaSyCMQMIGzB0XBstNMwhIAaFf3wjR_GX8aME",
-    authDomain: "memory-matrix-3dd3a.firebaseapp.com",
-    databaseURL: "https://memory-matrix-3dd3a.firebaseio.com",
-    projectId: "memory-matrix-3dd3a",
-    storageBucket: "memory-matrix-3dd3a.appspot.com",
-    messagingSenderId: "9749002468",
-    appId: "1:9749002468:web:1280e1e752dc38e84154f1",
-    measurementId: "G-QFDSQYCNQJ"
-};
-firebase.initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-var db = firebase.database().ref('users');
-
-const dbref = firebase.database().ref().child('users')
+// replace the uri string with your connection string.
+const uri = "mongodb+srv://curtisbellamy:trek4300@scoreboard-tg46s.mongodb.net/test?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 3000
 
@@ -54,8 +38,17 @@ app.post('/submitScore', (req, res) => {
         id: username,
         score: score
     }
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+        }
+        console.log('Connected...');
+        const collection = client.db("ScoreBoard").collection("users");
+        // perform actions on the collection object
+        collection.insertOne(user)
+        client.close();
+    });
 
-    dbref.push(user)
 
 
     // res.redirect(301, "leaderboard.html")
@@ -66,26 +59,34 @@ app.post('/submitScore', (req, res) => {
 
 
 app.get('/getUsers', (req, res) => {
-    var obj = []
 
-    dbref.on('value', getUserData);
-
-    function getUserData(data) {
-        var users = data.val()
-        var keys = Object.keys(users)
-        for (var i = 0; i < keys.length; i++) {
-            var k = keys[i]
-            var name = users[k].id
-            var score = users[k].score
-            obj.push({id: name, points: score})
+    MongoClient.connect(uri, function (err, client) {
+        if (err) {
+            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
         }
-    }
-    JSON.stringify(obj)
-    obj.sort(function(a, b){
-        return b.points - a.points;
+        console.log('Connected...');
+        const collection = client.db("ScoreBoard").collection("users");
+        // perform actions on the collection object
+
+        collection.find({}).toArray(function (err, results) {
+            var obj = []
+            for (let i = 0; i < results.length; i++) {
+                let name = results[i].id
+                let score = results[i].score
+                obj.push({ id: name, points: score })
+
+            }
+            JSON.stringify(obj)
+            obj.sort(function (a, b) {
+                return b.points - a.points;
+            });
+            userObj = obj.slice(0, 5)
+            res.send(userObj)
+
+        });
+
+        client.close();
     });
-    userObj = obj.slice(0, 5)
-    res.send(userObj)
 });
 
 
